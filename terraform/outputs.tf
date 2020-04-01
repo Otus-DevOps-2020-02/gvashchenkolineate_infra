47dec42481d8664992f1e045ab16e4b3d96399ba
Georgy Vashchenko <gvashchenko@lineate.com>
2020-04-01 11:50:55 +0600

    Terrafrom: Deactivate Load Balancers creation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "app_external_ip" {
   value = google_compute_instance.app[*].network_interface[0].access_config[0].nat_ip
 }
-output "load_balancer_ip" {
-  value = google_compute_global_forwarding_rule.reddit-app-fr.ip_address
-}
+
+//output "load_balancer_ip" {
+//  value = google_compute_global_forwarding_rule.reddit-app-fr.ip_address
+//}
